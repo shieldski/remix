e7f5c54a5be8757cd21d38d9678a3b1ae23e9b19
Logan McAnsh <logan@mcan.sh>
2022-06-09 11:20:56 -0400

    test: add safari and firefox to playwright
    Signed-off-by: Logan McAnsh <logan@mcan.sh>

--- integration/playwright.config.ts
+++ integration/playwright.config.ts
@@ -23,6 +23,18 @@ const config: PlaywrightTestConfig = {
         ...devices["Desktop Chrome"],
       },
     },
+    {
+      name: "firefox",
+      use: {
+        ...devices["Firefox"],
+      },
+    },
+    {
+      name: "webkit",
+      use: {
+        ...devices["Desktop Safari"],
+      },
+    },
   ],
 };
 
